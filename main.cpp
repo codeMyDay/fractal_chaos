e6a2117727a3d24c6d8b8a5befd81eb6875eac8a
DanilaDanila <manakov.danila@mail.ru>
2019-05-04 15:20:32 +0300

    add vec2.hpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <SFML/Graphics.hpp>
 #include <GL/gl.h>
 #include <unistd.h>
+#include "vec2.hpp"
+
+void glVertex2v(vec2 v)
+{
+    glVertex2f(v.x, v.y);
+}
 
 int main(int argc, char **argv)
 {
+    vec2 v0(1.f, 1.f);
+    vec2 v1(0.4f, -0.2f);
+
     sf::RenderWindow window(sf::VideoMode(700, 700), "Chaos");
     window.setFramerateLimit(30);
 
@@ -16,8 +25,16 @@ int main(int argc, char **argv)
         }
         window.clear();
 
+        glColor3f(1.0, 1.0, 1.0);
+        glBegin(GL_LINES);
+        glVertex2v(v0);
+        glVertex2v(v1);
+        glEnd();
+
         window.display();
         usleep(100000);
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) window.close();
     }
 
     return 0;
